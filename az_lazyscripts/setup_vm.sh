169e97bc533415ced929d02ccbdb94752661fa8e
ceej64 <32303858+ceej64@users.noreply.github.com>
2019-03-23 20:22:29 +0000

    Update setup_vm.sh

--- az_lazyscripts/setup_vm.sh
+++ az_lazyscripts/setup_vm.sh
@@ -2,7 +2,7 @@
 sudo apt-get -y update       
 #don't do apt-get upgrade because it does not work with AWS
 sudo apt -y install libmicrohttpd-dev libssl-dev cmake build-essential libhwloc-dev leafpad git xauth
-git clone https://github.com/ceej64/trtl-stak.git
+git clone https://github.com/ceej64/xmr-stak.git
 sudo sysctl -w vm.nr_hugepages=128
 cd trtl-stak
 chmod u+x az_lazyscripts/compile_and_config.sh
